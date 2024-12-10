d44e4170efd3fb70ce8fb6437addaef92b242526
Developeryasee <yaserarafath243@gmail.com>
2024-12-10 15:51:02 +0530

    add href to open app

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,21 @@
+'use client';
+
 export default function Home() {
+    const openLinkInNewTab = ( url ) => {
+        const newTab = window.open(url, '_blank', 'noopener,noreferrer');
+        if ( newTab ) newTab.opener = null;
+      }
     return(
+        <>
         <h1>Welcome Home!</h1>
+
+        <a href="https://nextapp-mu.vercel.app/authenticate">Click here</a>
+        <button onClick={()=>{
+            openLinkInNewTab("https://nextapp-mu.vercel.app/authenticate");
+
+        }}>
+        Press Here
+        </button>
+        </>
     )
 }
